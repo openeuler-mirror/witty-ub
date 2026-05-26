012a1eb3de5d63fdf43d61ec5ab000a9b1397d1f
liyuanrong <liyuanrong1@huawei.com>
2026-05-26 15:30:00 +0800

    delete unused code src/latency and unused data in data/
    Signed-off-by: liyuanrong <liyuanrong1@huawei.com>

--- src/plugins/latency/services/log_file.py
+++
@@ -1,9 +0,0 @@
-from re import U
-
-from latency.schemas.response import UploadLogFilesMsg
-
-
-class LogFileService:
-    @staticmethod
-    async def upload_log_files(kb_id: str) -> UploadLogFilesMsg:
-        return UploadLogFilesMsg(log_files_ids=[])
