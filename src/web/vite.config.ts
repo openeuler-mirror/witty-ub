03693c34fdd44549aa970d0f85dd55cdd1ff5d55
Eusford_0526 <sunzijian4@huawei.com>
2026-05-29 17:08:31 +0800

    web: delay and fault monitor; fit node v20.18.2

--- src/web/vite.config.ts
+++ src/web/vite.config.ts
@@ -6,13 +6,50 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      '/log_kb': {
+        target: 'http://127.0.0.1:9772',
+        changeOrigin: true,
+      },
+      '/log_file': {
+        target: 'http://127.0.0.1:9772',
+        changeOrigin: true,
+      },
+      '/log_parse_result': {
+        target: 'http://127.0.0.1:9772',
+        changeOrigin: true,
+      },
+      '/aggregated_event': {
+        target: 'http://127.0.0.1:9772',
+        changeOrigin: true,
+      },
+      '/anomalous_event': {
+        target: 'http://127.0.0.1:9772',
+        changeOrigin: true,
+      },
+      '/anomalous_event_chain': {
+        target: 'http://127.0.0.1:9772',
+        changeOrigin: true,
+      },
+      '/log_failure_event_result': {
+        target: 'http://127.0.0.1:9772',
+        changeOrigin: true,
+      },
+      '/failure_mode': {
+        target: 'http://127.0.0.1:9772',
+        changeOrigin: true,
+      },
+      '/task': {
+        target: 'http://127.0.0.1:9772',
+        changeOrigin: true,
+      },
     },
   },
 })
